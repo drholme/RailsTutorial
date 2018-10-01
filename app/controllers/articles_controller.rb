160b182474a6074dbf8999efa407d1acdace9cab
John Lange <lange.john.m@gmail.com>
2018-10-01 11:40:18 -0400

    Saving data in the controller w/whitelist

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,8 +3,10 @@ class ArticlesController < ApplicationController
     end
 
     def create
-        @article = Article.new(params[:article])
+        #whitelisting controller parameters
+        @article = Article.new(params.require(:article).permit(:title, :text))
 
+        #article.save will save model in the database
         @article.save
         redirect_to @article
     end
