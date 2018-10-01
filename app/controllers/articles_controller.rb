30486c5e888f166c3b6d25a7899efe6626f6fcf2
John Lange <lange.john.m@gmail.com>
2018-10-01 11:56:25 -0400

    Showing articles and beging to list them

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,25 @@
 class ArticlesController < ApplicationController
+    def index
+      #use Article.find to find the article
+      #and then pass in the id parameter
+      @articles = Article.all
+    end
+    
+    def show
+      #use Article.find to find the article
+      #and then pass in the id parameter
+      @article = Article.find(params[:id])
+    end
+   
     def new
     end
 
     def create
-        #whitelisting controller parameters
-        @article = Article.new(params.require(:article).permit(:title, :text))
+      #whitelisting controller parameters
+      @article = Article.new(params.require(:article).permit(:title, :text))
 
-        #article.save will save model in the database
-        @article.save
-        redirect_to @article
+      #article.save will save model in the database
+      @article.save
+      redirect_to @article
     end
 end
